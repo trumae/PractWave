83efbc53de5045fa92186e1d18bda7ca6d3c1fd7
Trumae <trumae@gmail.com>
2013-01-05 02:57:30 -0200

    adicao e edicao de clientes nao prontos

--- apps/Cliente/ClienteApp.cpp
+++ apps/Cliente/ClienteApp.cpp
@@ -40,6 +40,7 @@ WWidget* ClienteApp::getConteudo() {
 			       "     <h2>Adiciona Cliente</h2>"
 			       "   </div>"
 			       "</div>", Wt::XHTMLUnsafeText );
+  addButton->clicked().connect(this, &ClienteApp::formAdicionaCliente);
   
   WText *searchButton = new WText(
 				  "<div class='tile double bg-color-green'>"
@@ -144,7 +145,6 @@ void ClienteApp::processInicial(string inicial) {
 
 void ClienteApp::processCliente(int id, int idconta, string inicial) {
   CabureApplication *app = CabureApplication::cabureApplication();
-  Contabilidade *contabilidade = app->contabilidade_;
   clear();
 
   stack = new WStackedWidget();
@@ -159,7 +159,7 @@ void ClienteApp::processCliente(int id, int idconta, string inicial) {
   molduraConta->addWidget(new ContaCliente(this, idconta));
 
   stack->addWidget(molduraConta);
-  stack->addWidget(new WText("Teste"));
+  stack->addWidget(createFormCliente(app->clientes_->getClientePorId(id)));
 
   Wt::WAnchor *back = new Wt::WAnchor();
   back->setStyleClass("back-button big page-back");
@@ -169,7 +169,6 @@ void ClienteApp::processCliente(int id, int idconta, string inicial) {
   WText *conta = new WText("<li class='sticker sticker-color-blue'><a><i class='icon-clipboard-2'></i>Conta</a></li>", Wt::XHTMLUnsafeText);
   conta->setInline(true);
   conta->clicked().connect(this, &ClienteApp::showConta);
-
 
   WText *dados = new WText("<li class='sticker sticker-color-orange'><a><i class='icon-user'></i>Dados</a></li>", Wt::XHTMLUnsafeText);
   dados->setInline(true);
@@ -209,5 +208,226 @@ void ClienteApp::showConta(){
   stack->setCurrentIndex(0);
 }
 
+// Formularios de edicao e adicao
 
+void ClienteApp::formAdicionaCliente(){
+  Cliente cli;
+  clear();
+
+  Wt::WAnchor *back = new Wt::WAnchor();
+  back->setStyleClass("back-button big page-back");
+  back->setInline(true);
+  back->clicked().connect(this, &ClienteApp::init);
+
+  WPushButton *btnSave = new WPushButton();
+  btnSave->setStyleClass("btn btn-primary");
+  btnSave->setText("Adicionar");
+  btnSave->clicked().connect(this, &ClienteApp::adicionaCliente);
+  
+  WPushButton *btnCancel = new WPushButton("Cancelar");
+  btnCancel->setStyleClass("btn");
+  btnCancel->clicked().connect(this, &ClienteApp::init );
+
+  WTemplate *t = new WTemplate(this);
+  t->setTemplateText(
+		     "<div class='page secondary'>"
+		     "   <div class='page-header'>"
+		     "      <div class='page-header-content'>"
+		     "          <h1>${titulo}</h1>"
+		     "          ${backButton}"
+		     "      </div>"
+		     "   </div>"
+		     "   <div class='page-region'>"
+		     "      <div class='page-region-content'>"
+		     "      ${conteudo}"
+		     "      <div class=grid>"
+		     "        <div class='row'>"
+		     "         ${save}${cancel}"
+		     "        </div>"
+		     "      </div>"
+		     "      </div>"
+		     "   </div>"
+		     "</div>", Wt::XHTMLUnsafeText);		 
+  
+  t->bindString("titulo", "Cadastra novo cliente");
+  t->bindWidget("save", btnSave);
+  t->bindWidget("cancel", btnCancel);
+  t->bindWidget("backButton", back);
+  t->bindWidget("conteudo", createFormCliente(cli));
+}
+
+WWidget* ClienteApp::createFormCliente(Cliente cli) {
+  editandoId = cli.id; //armazena id que podera ser salvo
+  editNome_ = new Wt::WLineEdit();
+  editNome_->setText(WString(cli.nome,UTF8));
+  editNome_->setMaxLength(100);
+  editEndereco_ = new Wt::WLineEdit();
+  editEndereco_->setText(WString(cli.endereco,UTF8));
+  editBairro_ = new Wt::WLineEdit();
+  editBairro_->setText(WString(cli.bairro, UTF8));
+  editCidade_ = new Wt::WLineEdit();
+  editCidade_->setText(WString(cli.cidade,UTF8));
+  editEstado_ = new Wt::WLineEdit();
+  editEstado_->setText(WString(cli.estado, UTF8));
+  editCEP_ = new Wt::WLineEdit();
+  editCEP_->setText(WString(cli.cep,UTF8));
+  editTelefone_ = new Wt::WLineEdit();
+  editTelefone_->setText(WString(cli.telefone, UTF8));
+  editCelular_ = new Wt::WLineEdit();
+  editCelular_->setText(WString(cli.celular,UTF8));
+  editEmail_ = new Wt::WLineEdit();
+  editEmail_->setText(WString(cli.email,UTF8));
+  editObservacao_ = new Wt::WTextArea();
+  editObservacao_->setText(WString(cli.observacao, UTF8));
+      
+  WTemplate *t = new WTemplate();
+  t->setTemplateText(
+		     "<div class='grid'>"
+		     "  <div class='row'>"
+		     "    <div class='span2'>Nome</div>"
+		     "    <div class='input-control text span6'>"
+		     "        ${nome}"
+		     "        <button class='helper'></button>"
+		     "    </div>"
+		     "    <div class=''span1>(obrigat&oacute;rio)</div>"
+		     "  </div>"
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>Endere&ccedil;o</div>"
+		     "    <div class='input-control text span6'>"
+		     "        ${endereco}"
+		     "        <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>Bairro</div>"
+		     "    <div class='input-control text span6'>"
+		     "        ${bairro}"
+		     "        <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>Cidade</div>"
+		     "    <div class='input-control text span6'>"
+		     "          ${cidade}"
+		     "        <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>Estado</div>"
+		     "    <div class='input-control text span6'>"
+		     "        ${estado}"
+		     "        <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>CEP</div>"
+		     "    <div class='input-control text span6'>"
+		     "       ${CEP}"
+		     "       <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"		     
+
+		     "  <div class='row'>"
+		     "    <div class='span2'>Telefone</div>"
+		     "    <div class='input-control text span6'>"
+		     "       ${telefone}"
+		     "       <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+		     
+		     "  <div class='row'>"
+		     "    <div class='span2'>Celular</div>"
+		     "    <div class='input-control text span6'>"
+		     "       ${celular}"
+		     "       <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+		   
+		     "  <div class='row'>"
+		     "    <div class='span2'>E-mail</div>"
+		     "    <div class='input-control text span6'>"
+		     "       ${email}"
+		     "       <button class='helper'></button>"
+		     "    </div>"
+		     "  </div>"
+		     
+		     "  <div class='row'>"
+		     "    <div class='span2'>Observa&ccedil;&atilde;o</div>"
+		     "    <div class='input-control textarea span6'>"
+		     "       ${observacao}"		 
+		     "    </div>"
+		     "  </div>", Wt::XHTMLUnsafeText);
+
+  t->bindWidget("nome", editNome_);
+  t->bindWidget("endereco", editEndereco_);
+  t->bindWidget("bairro", editBairro_);
+  t->bindWidget("cidade", editCidade_);
+  t->bindWidget("estado", editEstado_);
+  t->bindWidget("CEP", editCEP_);
+  t->bindWidget("telefone", editTelefone_);
+  t->bindWidget("celular", editCelular_);
+  t->bindWidget("email", editEmail_);
+  t->bindWidget("observacao", editObservacao_);
+
+  return t;
+}
+
+void ClienteApp::adicionaCliente(){
+  CabureApplication *cabure = CabureApplication::cabureApplication();
+  Clientes *c = cabure->clientes_;
+  
+  if(editNome_->text().toUTF8() != "") {
+    Cliente cli(editNome_->text().toUTF8(),
+		editEndereco_->text().toUTF8(),
+		editBairro_->text().toUTF8(),
+		editCidade_->text().toUTF8(),
+		editEstado_->text().toUTF8(),
+		editCEP_->text().toUTF8(),
+		editTelefone_->text().toUTF8(),
+		editCelular_->text().toUTF8(),
+		editEmail_->text().toUTF8(),
+		editObservacao_->text().toUTF8());
+    
+    if(c->adiciona(cli)) {
+      init();
+      //setMessage("Novo cliente adicionado com sucesso!", "alert-success");
+    } else {
+      //setMessage("Erro ao adicionar novo cliente", "alert-error");
+    }
+  } else {
+    //setMessage("O nome do cliente &eacute; obrigat&oacute;rio!", "alert-error");
+  }
+}
+
+void ClienteApp::saveCliente(){
+  CabureApplication *cabure = CabureApplication::cabureApplication();
+  Clientes *c = cabure->clientes_;
+  
+  if(editNome_->text().toUTF8() != "") {
+    Cliente cli(editNome_->text().toUTF8(),
+		editEndereco_->text().toUTF8(),
+		editBairro_->text().toUTF8(),
+		editCidade_->text().toUTF8(),
+		editEstado_->text().toUTF8(),
+		editCEP_->text().toUTF8(),
+		editTelefone_->text().toUTF8(),
+		editCelular_->text().toUTF8(),
+		editEmail_->text().toUTF8(),
+		editObservacao_->text().toUTF8());
+    cli.id = editandoId;
+    if(c->salvar(cli)) {
+      init();
+      //setMessage("Cliente editado com sucesso!", "alert-success");
+    } else {
+      //setMessage("Erro ao editar novo cliente", "alert-error");
+    }
+  } else {
+    //setMessage("O nome do cliente &eacute; obrigat&oacute;rio!", "alert-error");
+  }
+}
 
--- apps/Cliente/ClienteApp.h
+++ apps/Cliente/ClienteApp.h
@@ -3,28 +3,54 @@
 
 #include <Wt/WContainerWidget>
 #include <Wt/WStackedWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WTextArea>
 #include "../App.h"
 #include "../../logic/Clientes.h"
 
 class ClienteApp : public App {
-private:
-	Wt::WStackedWidget *stack;
-protected:
-    virtual WWidget* getConteudo();
-    virtual std::string getTitulo();
+ private:
+  Wt::WStackedWidget *stack;
+ protected:
+  virtual WWidget* getConteudo();
+  virtual std::string getTitulo();
+  
+  void processInicial(std::string inicial);
+  void processCliente(int id, int idconta, std::string inicial);
+  
+  void showDados();
+  void showConta();
+  
+  Wt::WLineEdit *buscaEdit;
+  Wt::WPushButton *buscaBtn;
+  int editandoId;
+  Wt::WLineEdit *editNome_;
+  Wt::WLineEdit *editEndereco_;
+  Wt::WLineEdit *editBairro_;
+  Wt::WLineEdit *editCidade_;
+  Wt::WLineEdit *editEstado_;
+  Wt::WLineEdit *editCEP_;
+  Wt::WLineEdit *editTelefone_;
+  Wt::WLineEdit *editCelular_;
+  Wt::WLineEdit *editEmail_;
+  Wt::WTextArea *editObservacao_;
 
-    void processInicial(std::string inicial);
-    void processCliente(int id, int idconta, std::string inicial);
+  void formAdicionaCliente();
+  Wt::WWidget* createFormCliente(Cliente cli);
 
-	void showDados();
-	void showConta();
-public:
-    ClienteApp(Wt::WContainerWidget *parent);
+  void adicionaCliente();
+  void saveCliente();
+  void buscaCliente();
+
+ public:
+  ClienteApp(Wt::WContainerWidget *parent);
 };
 
 class TileCliente : public Wt::WContainerWidget {
-public:
-    TileCliente();
+ public:
+  TileCliente();
 };
 
 #endif
+
