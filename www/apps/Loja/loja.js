a15d77e9350752e2c9add396376e021a8df826cd
willian <willianfarago@gmail.com>
2013-01-18 18:14:56 -0200

    ajustes loja

--- www/apps/Loja/loja.js
+++ www/apps/Loja/loja.js
@@ -1,6 +1,12 @@
 function initLoja(){
+    ativaCarousel();
 	$(".conteudoApp").hide();
 	
+	$(".goLoja").click(function(){
+		$(".conteudoApp").hide();
+		$("#Loja").show();
+	});
+
 	$("#TileCalculadora").click(function(){
 		$(".conteudo").hide();
 		$("#AppCalculadora").show();
--- www/javascript/practwave.js
+++ www/javascript/practwave.js
@@ -13,3 +13,21 @@ function ativaTilesSlider () {
 function initMainMenu() {
     ativaTilesSlider();
 }
+
+function ativaCarousel(){
+    var allCarousels = $('[data-role=carousel], .carousel');
+    allCarousels.each(function (index, carousel) {
+        var params = {};
+        $carousel = $(carousel);
+        params.auto         = $carousel.data('paramAuto');
+        params.period       = $carousel.data('paramPeriod');
+        params.duration     = $carousel.data('paramDuration');
+        params.effect       = $carousel.data('paramEffect');
+        params.direction    = $carousel.data('paramDirection');
+        params.markers      = $carousel.data('paramMarkers');
+        params.arrows       = $carousel.data('paramArrows');
+        params.stop         = $carousel.data('paramStop');
+
+        $carousel.Carousel(params);
+    })
+}
