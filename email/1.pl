7dede1c89193c76d3cb03cdace6e592136515a68
trumae <trumae@gmail.com>
2013-08-08 20:32:37 -0700

    Delete 1.pl
    del teste.pl

--- email/1.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -W
-
-use strict;
-use CabureEmail;
-
-my $toemail = $ARGV[0];
-
-my $corpo="Teste de mail pelo script de notificacao";
-CabureEmail::sendEmail($toemail,"teste", $corpo);
